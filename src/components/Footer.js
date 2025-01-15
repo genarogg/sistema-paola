f54e98c36b8ab44f880b940d52dd56cac792d9a6
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-15 13:58:23 -0400

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./styles/Footer.css"
+
+const Footer = () =>{
+    return(
+        <footer>
+        <div className="centrado">
+          <p>
+            De LATAM con{" "}
+            <span role="img" aria-label="sheep">
+              💜
+            </span>{" "}
+            para el mundo | Genarogg
+          </p>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
